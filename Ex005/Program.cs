866bd4c95eb09efdab3428ebd2ee72d95534c32e
AnnaRandom <alekseeva.anna@yahoo.com>
2023-02-21 21:28:14 +0300

    Added Exercise 005 & 002

---
+++ Ex005/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые 
+// числа в промежутке от -N до N.
+//   4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+//  2 -> " -2, -1, 0, 1, 2"
+
+// откуда N - задаем пользователь
+// куда выводим результат - терминал
+
+// 2. Алгоритм в Diagram.drawio.png
+// 
+int N;
+//Console.Writeline("Введите N");
+//N = int.Parse(Console.Readline[]);
+N = 5;
+int index;
+index = -N;
+while (index <= N)
+{
+    Console.Writeline (index + " ");
+    index = index + 1;
+}
+
+Console.Writeline("END");
