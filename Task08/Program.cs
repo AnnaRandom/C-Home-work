446218b648494a766e2f01943f0532d7e0289ddd
AnnaRandom <alekseeva.anna@yahoo.com>
2023-02-21 22:26:39 +0300

    Added Task 08 of Homework

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int n = 24;
+int index = 1;
+while (index < n)
+{
+    if (index % 2 == 0)
+    {
+    Console.WriteLine (index);
+    }
+    index = index + 1;
+}
+ Console.WriteLine ("END");
