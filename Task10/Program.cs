9f7d85414b0c1c470840014743a436d77cbf0498
AnnaRandom <alekseeva.anna@yahoo.com>
2023-02-23 18:45:02 +0300

    exersice 10 of homework added

---
+++ Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+System.Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.Readline());
+int a = value / 100;
+int c = value % 10;
+int b = value / 10 - a * 10;
+
+System.Console.WriteLine(b);
