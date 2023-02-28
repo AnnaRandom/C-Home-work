836824c1a51c554707e94b9cdca9acae67eb3eca
AnnaRandom <alekseeva.anna@yahoo.com>
2023-02-28 16:06:55 +0300

    Resolved Task10

---
+++ Lecure03/Ex01/Program.cs
@@ -0,0 +1,88 @@
+﻿// Groups of methods
+
+void Method1()
+{
+ Console.WriteLine("Author...");   
+}
+Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg:"Text of message");
+// Вид 2-1 - повторять определенное количество раз
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21 ( msg: "Tekst", count: 4);
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4
+//string Method4(int count, string c)
+//{
+//int i = 0;
+//string result = String.Empty;
+//for(int i = 0; i < count; i++)
+    //while (i < count)
+    {
+  //      result = result + c;
+    //    i++;
+    //}
+    //return result;
+//}
+//string res = Method4(10, "asdf");
+//Console.WriteLine(res);
+
+//for (int i = 2; i <=10; i++)
+//{
+   // for (int j = 2; j <=0; j++)
+    //{
+    //    Console.WriteLine ($"{i}*{j} = {i*j}");
+    //}
+    //Console.WriteLine();
+//}
+
+int[] array = { 1, 5, 4, 3, 2, 6,7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int  count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+            Console.WriteLine ($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j <array.Length; j++)
+        {
+            if (array[j]< array[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+          int termporary = array[i];
+          array[i] = array [minPosition];
+          array[minPosition] = temporary;
+    }
+}
+}
+//PrintArray(arr);
--- Task10/Program.cs
+++ Task10/Program.cs
@@ -4,10 +4,20 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
-System.Console.WriteLine("Введите число: ");
-int value = Convert.ToInt32(Console.Readline());
-int a = value / 100;
-int c = value % 10;
-int b = value / 10 - a * 10;
 
-System.Console.WriteLine(b);
+int SecondDigit(int num)
+{
+   return (num % 100) / 10;
+}
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>99 && number<1000)
+{
+   int result = SecondDigit(number);
+   Console.WriteLine(result);
+}
+else
+{
+   Console.WriteLine("Число не трёхзначное");
+}
