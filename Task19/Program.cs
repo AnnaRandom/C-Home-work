c4dd8efe0a840bf1eb037a024a2a90810f0550a9
AnnaRandom <alekseeva.anna@yahoo.com>
2023-03-16 10:44:48 +0300

    added solution for task19

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+bool palindromAdverseReader(long numC)
+{
+  if (numC < 0) return false;
+  if (numC == 0) return true;
+  string numAs = Convert.ToString(numC);
+  return numAs.SequenceEqual(numAs.Reverse());
+}
+Console.Write("Введите число для проверки на палиндром: ");
+long testMe = long.Parse(Console.ReadLine());
+
+Console.Write("Результат: ");
+
+Console.WriteLine(palindromAdverseReader(testMe));
